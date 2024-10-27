a62d7b60086db8574c377a3c448958bf3033a316
airvt1x's PC <airat3552@gmail.com>
2024-10-27 13:49:30 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
-    imports: [],
+  imports: [],
 })
 export class AppModule {}
